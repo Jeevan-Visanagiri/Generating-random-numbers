8dbc866a9b334856d79e1ccd94df47e36db0ca41
Jeevan Visanagiri <65053351+Jeevan-Visanagiri@users.noreply.github.com>
2020-07-22 08:38:30 +0530

    Add files via upload

---
+++ source/script.js
@@ -0,0 +1,13 @@
+function random(num)
+{
+  var x=Math.floor( Math.random()*10);
+      if(num==1)
+        return num;
+        else 
+      {
+          return x=x+random(num-1)*10;
+       }
+      }     
+
+
+console.log('The 8 digit Random number are '+random(8));
