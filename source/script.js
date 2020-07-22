937649747bdc266198f96cfef56d7a9e826ba6f7
Jeevan Visanagiri <65053351+Jeevan-Visanagiri@users.noreply.github.com>
2020-07-22 08:37:26 +0530

    Delete script.js

--- source/script.js
+++
@@ -1,4 +0,0 @@
-let random=(mult) =>{s
-    return ((Math.floor(Math.random()*mult)>=10000000) & (Math.floor(Math.random()*mult) <=99999999) )?Math.floor(Math.random()*mult) : random(mult) 
-  } 
-console.log('The 8 digit Random number are '+random(10));
